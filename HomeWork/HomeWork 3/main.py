c9074b4562721995d864682f0e23e62a472eb52b
Vladislav Verin <dippicaka@gmail.com>
2023-04-20 15:33:55 +0300

    Homework 3 extended

--- HomeWork/HomeWork 3/main.py
+++ HomeWork/HomeWork 3/main.py
@@ -8,7 +8,7 @@
 # nearest_num = 0
 # difference = 1000000
 #
-# list_numb = [random.randint(1, 100) for i in range(n)]
+# list_numb = [random.randint(1, 10) for i in range(n)]
 # print(list_numb)
 #
 # for i in range(n):
@@ -23,6 +23,26 @@
 # else:
 #     print(f"Ближайшее число: {nearest_num}")
 
+# Вариант решения из 4 семинара
+
+# import random
+#
+# size = int(input("Введите количество чисел в списке: "))
+# max_limit = int(input("Введите верхний предел: "))
+# min_limit = int(input("Введите нижний предел: "))
+# desired_number = int(input("Введите искомое число: "))
+# list_num = [random.randint(min_limit, max_limit) for i in range(size)]
+# closest = list_num[len(list_num) - 1]
+#
+# print(list_num)
+# count = list_num.count(desired_number)
+#
+# if count < 1:
+#     for i in list_num:
+#         if abs(desired_number - i) < abs(desired_number - closest):
+#             closest = i
+# print(f"Число {desired_number} встречается {count} раз/раза" if count >
+#                                                                 0 else f"Ближайшее число к искомому: {closest}")
 
 # Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
 # В случае с английским алфавитом очки распределяются так:A, E, I, O, U, L, N, S, T, R – 1 очко;
@@ -33,28 +53,68 @@
 # Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 # либо только русские буквы.
 
+# scrabble_en = {'A': '1', 'E': '1', 'I': '1', 'O': '1', 'U': '1', 'L': '1', 'N': '1', 'S': '1', 'T': '1', 'R': '1',
+#                'D': '2', 'G': '2', 'B': '3', 'C': '3', 'M': '3', 'P': '3', 'F': '4', 'H': '4', 'V': '4', 'W': '4',
+#                'Y': '4', 'K': '5', 'J': '8', 'X': '8', 'Q': '10', 'Z': '10'}
+# scrabble_ru = {'А': '1', 'В': '1', 'Е': '1', 'И': '1', 'Н': '1', 'О': '1', 'Р': '1', 'С': '1', 'Т': '1', 'Д': '2',
+#                'К': '2', 'Л': '2', 'М': '2', 'П': '2', 'У': '2', 'Б': '3', 'Г': '3', 'Ё': '3', 'Ь': '3', 'Я': '3',
+#                'Й': '4', 'Ы': '4', 'Ж': '5', 'З': '5', 'Х': '5', 'Ц': '5', 'Ч': '5', 'Ш': '8', 'Э': '8', 'Ю': '8',
+#                'Ф': '10', 'Щ': '10', 'Ъ': '10'}
+# points_list = []
+# points = 0
+#
+# print(word := input("Введите слово: "), list(word))
+#
+# for i in range(len(word)):
+#     if not word[i] in scrabble_en:
+#         print(scrabble_ru[list(word)[i]], end=" ")
+#         points_list += scrabble_ru[list(word)[i]]
+#         points += int(points_list[i])
+#
+#     else:
+#         print(scrabble_en[list(word)[i]], end=" ")
+#         points_list += scrabble_en[list(word)[i]]
+#         points += int(points_list[i])
+#
+# print(f"\n{points_list}")
+# print(f"\n{points}")
 
-scrabble_en = {'A': '1', 'E': '1', 'I': '1', 'O': '1', 'U': '1', 'L': '1', 'N': '1', 'S': '1', 'T': '1', 'R': '1',
-               'D': '2', 'G': '2', 'B': '3', 'C': '3', 'M': '3', 'P': '3', 'F': '4', 'H': '4', 'V': '4', 'W': '4',
-               'Y': '4', 'K': '5', 'J': '8', 'X': '8', 'Q': '10', 'Z': '10'}
-scrabble_ru = {'А': '1', 'В': '1', 'Е': '1', 'И': '1', 'Н': '1', 'О': '1', 'Р': '1', 'С': '1', 'Т': '1', 'Д': '2',
-               'К': '2', 'Л': '2', 'М': '2', 'П': '2', 'У': '2', 'Б': '3', 'Г': '3', 'Ё': '3', 'Ь': '3', 'Я': '3',
-               'Й': '4', 'Ы': '4', 'Ж': '5', 'З': '5', 'Х': '5', 'Ц': '5', 'Ч': '5', 'Ш': '8', 'Э': '8', 'Ю': '8',
-               'Ф': '10', 'Щ': '10', 'Ъ': '10'}
-points_list = []
-points = 0
+# Вариант решения из 4 семинара
 
-print(word := input("Введите слово: "), list(word))
+# scrabble = {"AEIOULNSTRАВЕИНОРСТ": 1,
+#             "DGДКЛМПУ": 2,
+#             "BCMPБГЁЬЯ": 3,
+#             "FHVWYЙЫ": 4,
+#             "KЖЗХЦЧ": 5,
+#             "JXШЭЮ": 8,
+#             "QZФЩЪ": 10}
+#
+# word = input("Введите слово: ")
+# total = 0
+#
+# for letter in word.upper():
+#     for letters in scrabble.keys():  # ... in scrabble: == ... in scrabble.keys():
+#         if letter in letters:
+#             total += scrabble.get(letters)
+#             break
+# print(f"Слово {word.upper()} набирает {total} баллов")
 
-for i in range(len(word)):
-    if not word[i] in scrabble_en:
-        print(scrabble_ru[list(word)[i]], end=" ")
-        points_list += scrabble_ru[list(word)[i]]
-        points += int(points_list[i])
+# Еще один
 
-    else:
-        print(scrabble_en[list(word)[i]], end=" ")
-        points_list += scrabble_en[list(word)[i]]
-        points += int(points_list[i])
+scrabble = {1: "AEIOULNSTRАВЕИНОРСТ",
+            2: "DGДКЛМПУ",
+            3: "BCMPБГЁЬЯ",
+            4: "FHVWYЙЫ",
+            5: "KЖЗХЦЧ",
+            8: "JXШЭЮ",
+            10: "QZФЩЪ"}
 
-print(f"\n{points}")
+word = input("Введите слово: ")
+total = 0
+
+for letter in word.upper():
+    for points, letters in scrabble.items():
+        if letter in letters:
+            total += points
+            break
+print(f"Слово {word.upper()} набирает {total} баллов")
