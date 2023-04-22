5bc5c66739fdf975a3459d436d3cb27571beb806
Vladislav Verin <dippicaka@gmail.com>
2023-04-22 15:21:07 +0300

    Homework 4

--- Seminar/Lesson 4/main.py
+++ Seminar/Lesson 4/main.py
@@ -12,11 +12,29 @@
 
 # Альтернатива
 
-text = list(input("Введите строку: "))
+# text = list(input("Введите строку: "))
+#
+# count_dict = {}
+#
+# for char in text:
+#     count_dict[char] = count_dict.get(char, -1) + 1
+#     # через запятую указываем то, что нам нужно вывести, если нет искомого ключа
+#     print(f"{char}" if count_dict.get(char) == 0 else f"{char}_{count_dict.get(char)}", end=" ")
 
-count_dict = {}
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
 
-for char in text:
-    count_dict[char] = count_dict.get(char, -1) + 1
-    # через запятую указываем то, что нам нужно вывести, если нет искомого ключа
-    print(f"{char}" if count_dict.get(char) == 0 else f"{char}_{count_dict.get(char)}", end=" ")
+# my_string = input("Введите строку: ")
+my_string = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells".upper()
+words = set()
+
+for i in my_string.upper():
+    words.update(my_string.split())
+print(len(words))
+
+
+
+
